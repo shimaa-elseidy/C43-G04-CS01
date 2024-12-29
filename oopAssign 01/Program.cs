b555151bbfec88f3ab759aa5263760cffed2de0e
Shimaa Elseidy <shimaaelseidy123@gmail.com>
2024-12-29 16:32:49 +0200

    Part 01 Q01

--- oopAssign 01/Program.cs
+++ oopAssign 01/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Part 01
+            /* 1.Create a struct called "Point" to represent a 2D point with 
+                properties "X" and "Y". Write a C# program that takes two points 
+                as input from the user and calculates the distance between them.*/
+
+
+            #endregion
         }
     }
 }
