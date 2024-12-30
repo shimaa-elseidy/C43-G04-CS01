3a6e77e8e7c75225478d4247895c71ebbd9873d1
Shimaa Elseidy <shimaaelseidy123@gmail.com>
2024-12-30 15:50:53 +0200

    OOP overview

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -7,47 +7,62 @@
     {
             static void Main(string[] args)
             {
-                #region Struct01
-                // Struct  : Value type ==> Stack
+            #region Struct01
+            // Struct  : Value type ==> Stack
 
-                //Employee
-                //Id , Name ,Salary 
+            //Employee
+            //Id , Name ,Salary 
 
-                //Point P01;// create data mn type point  // 3mlna kda declare ll object direct msh ll refrence from type 'Point'.
-                //P01: object 
-                //Allocate unintialized 8 bytes at stack ===> ( x==> 4bytes,y ==> 4bytes).
-                //Console.WriteLine(P01.x);// unassigned
+            //Point P01;// create data mn type point  // 3mlna kda declare ll object direct msh ll refrence from type 'Point'.
+            //P01: object 
+            //Allocate unintialized 8 bytes at stack ===> ( x==> 4bytes,y ==> 4bytes).
+            //Console.WriteLine(P01.x);// unassigned
 
-                //P01.X = 1;
-                //P01.Y = 2;
-                //Console.WriteLine(P01.x );
-                //Console.WriteLine(P01.y);
+            //P01.X = 1;
+            //P01.Y = 2;
+            //Console.WriteLine(P01.x );
+            //Console.WriteLine(P01.y);
 
-                //P01 = new Point();// CLR : paramterless , has defalut value
-                //Console.WriteLine(P01);// namespace Demo.point
+            //P01 = new Point();// CLR : paramterless , has defalut value
+            //Console.WriteLine(P01);// namespace Demo.point
 
-                // New for : Used for Select Constructor
-                //Console.WriteLine(P01.x);// 0 ==> default value
+            // New for : Used for Select Constructor
+            //Console.WriteLine(P01.x);// 0 ==> default value
 
-                // b3d ama 3mlt constructor b values gowah
-                //Console.WriteLine(P01.x);// 20  kda la5eet el CLR elly bytb3 el default value.
+            // b3d ama 3mlt constructor b values gowah
+            //Console.WriteLine(P01.x);// 20  kda la5eet el CLR elly bytb3 el default value.
 
-                //P01 = new Point(2,3);
-                //Console.WriteLine(P01.X);//2
-                //Console.WriteLine(P01.Y);//3
+            //P01 = new Point(2,3);
+            //Console.WriteLine(P01.X);//2
+            //Console.WriteLine(P01.Y);//3
 
 
-                //P01.PrintPoint();
-                //Console.WriteLine(P01.ToString()); //======name space
+            //P01.PrintPoint();
+            //Console.WriteLine(P01.ToString()); //======name space
 
-                // b3d ama 3mlt override 
-                //Console.WriteLine(P01.ToString()); //(1.2)
+            // b3d ama 3mlt override 
+            //Console.WriteLine(P01.ToString()); //(1.2)
 
-                //Point[] points = new Point[10];// 10 instance 
+            //Point[] points = new Point[10];// 10 instance 
 
-                #endregion
-            }
+            #endregion
+            #region OOP overview
+            //OOP : object oriented programming
+            //    : programming paradigm 
+            //    : the best paradigm used for build any business
+            //    : Class ==> Blueprint of the object.
+            //    : Object===> specific instance from class
+            //     =====================================================
+            //     : Has 4 pillars
+            //       1. Encapsulation
+            //       2. Inheritance
+            //       3. Ploymorphism
+            //       4. Abstraction
+            //     =====================================================
+
+
+            #endregion
+        }
     }
 }
-
-
+                                                                                            
